98bc12d76496d344030fbfecb9b0cba2d2ec202a
Christopher Loonam <christopherloonam@ChristosMacBook.home>
2015-08-18 23:21:22 -0400

    Initial Commit

---
+++ CLFileNavigator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CLFileNavigator
+//
+//  Created by Christopher Loonam on 8/18/15.
+//  Copyright (c) 2015 Christopher Loonam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CLFileNavigator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CLFileNavigator
+//
+//  Created by Christopher Loonam on 8/18/15.
+//  Copyright (c) 2015 Christopher Loonam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
